2d9b463b45ffedaf72c18d19985565ce5b50553b
themanda <jorge@bsdchile.cl>
2013-11-11 23:13:51 +0100

    modified:   test_nshell.sh

--- test_nshell.sh
+++ test_nshell.sh
@@ -15,5 +15,16 @@ assert "file_exist ./assert.sh" "YES"
 assert "iam_running test_nshell.sh" "NOT"
 assert "get_os_name" "Linux"
 assert "find_string_in_file trim nshell.sh" "YES"
+assert "continue_question" " Do you want continue? (y/n)? " "y"
+assert_raises "continue_question" 0 "y"
+assert_raises "continue_question" 1 "n"
+#assert "get_color red" "\033[0;31m"
+assert "color_string red hola" "\033[0;31mhola\033[0m"
+
+
+
+
+
+
 
 assert_end
