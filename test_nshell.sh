902079699dc2e095dbc94aa1a95d36d1185e585f
themanda <jorge@bsdchile.cl>
2013-11-11 21:52:35 +0100

    modified:   test_nshell.sh

--- test_nshell.sh
+++ test_nshell.sh
@@ -3,3 +3,24 @@
 # 
 . ./assert.sh
 
+space_fill
+zero_fill
+timestamp
+get_date
+usage
+exit_message
+check_for_cli_args
+continue_question
+require_user
+trim
+rtrim
+ltrim
+lower
+upper
+file_exist
+running
+iam_running
+find_string_in_file
+get_os_name
+get_color
+color_string
